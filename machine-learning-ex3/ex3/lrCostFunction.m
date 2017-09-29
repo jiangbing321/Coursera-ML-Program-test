98eb3dbd8f63a3e08f7aebde89acde543e20f17a
Jiangbing Chen (jiangbch) <jiangbch@cisco.com>
2017-09-29 09:13:07 +0800

    finish lecture 4th's  ex3

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -38,8 +38,32 @@ grad = zeros(size(theta));
 
 
 
+h_x=sigmoid(X*theta);
+log_h_x=log(h_x);
+log_1_h_x = log(1-h_x);
+
+_y_log_h_x=-y.*log_h_x;
+
+one_y_log_1_h_x=(1-y).*log_1_h_x;
+
+J1=sum(_y_log_h_x-one_y_log_1_h_x)/m ;
+
+theta1 = theta; % we should exclude theat0
+theta1(1) = 0;
+reg_part = sum(theta1.^2)*lambda/(2*m);
+
+J = J1 + reg_part;
 
 
+
+temp=bsxfun(@times,(h_x-y),X);
+grad_sum=sum(temp);
+grad1=grad_sum'/m;
+
+
+grad2 = grad1 + lambda*theta/m;
+grad2(1) = grad2(1) - lambda*theta(1)/m;
+grad = grad2;
 
 
 
--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,10 +21,16 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+X = [ones(m, 1) X]; % --> 5000*401
+
+lay2 = sigmoid(X*Theta1'); %5000*401 X 401*25 =  5000*25
+lay2_result=[ones(m, 1) lay2]; % --> 5000*26
+lay3 = sigmoid(lay2_result*Theta2'); % 5000*26 X 26*10 = 5000 * 10
 
 
+[p1,p2]=max(lay3, [], 2);  % max
 
-
+p=p2;
 
 
 
